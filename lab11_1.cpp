5e5623ba3ce73d0773e71115393259b23175d897
Tiw1 <kkhowphuns@gmail.com>
2024-12-20 14:36:59 +0700

    lab11_1 Complete

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,26 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+string Grade[] = {"A", "B+", "B", "C+", "C", "D+", "D", "F", "W"};;
+int main(){
+    cout << "Press Enter 3 times to reveal your future.";
+    for ( int i = 0; i < 3; i++)
+    {
+        cin.get();
+    }
+    srand(time(0));
+    
+    int randomIndex = rand() % 9;
+
+    cout << "You will get " << Grade[randomIndex] << " in this 261102." << endl;
+
+    return 0;
+
+
+
+
+}
 // cout << "Press Enter 3 times to reveal your future.";
 // cout << "You will get A in this 261102.";
